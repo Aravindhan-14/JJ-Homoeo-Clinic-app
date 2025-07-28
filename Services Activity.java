ad626f92e4e0acedab3342f7396231002577fbe0
M Aravindhan 07 <aravindh2k05@gmail.com>
2025-07-28 21:57:32 +0530

    Create Services Activity.java

---
+++ Services Activity.java
@@ -0,0 +1,12 @@
+package com.example.jjhomeoclinic;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ServicesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services);
+    }
+}
